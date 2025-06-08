0300e76078ab0b0355955897da9fd3eba3fef1fc
MateuszBrankiewicz <bronczek522@gmail.com>
2025-06-08 21:05:49 +0200

    add user model

---
+++ backend/TeamTaskPlanner/Model/User.cs
@@ -0,0 +1,10 @@
+namespace TeamTaskPlanner.Model;
+
+public class User
+{
+  private int Id { get; set; }
+  public string Name { get; set; }
+  public string Email { get; set; }
+  public string Password { get; set; }
+
+}
