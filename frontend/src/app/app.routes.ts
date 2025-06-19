f89dac786fa2b66e7205d0444cbd3fe04a1a508b
MateuszBrankiewicz <bronczek522@gmail.com>
2025-06-19 16:12:31 +0200

    add routing and create dashboard module

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,6 +3,10 @@ import {RouterModule, Routes} from '@angular/router';
 
 export const routes: Routes = [
   {
+    path : '',
+    loadChildren: () => import('./modules/dashboard/dashboard.module').then((m) => m.DashboardModule),
+  }
+  ,{
     path: 'auth',
     loadChildren: () => import('./modules/auth/auth.module').then((m) => m.AuthModule),
   }
