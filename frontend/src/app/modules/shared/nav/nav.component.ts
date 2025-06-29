a712cf941c1bc83dc177042a0a07c8d765ff5440
MateuszBrankiewicz <bronczek522@gmail.com>
2025-06-29 12:26:26 +0200

    fix componnents size bug

--- frontend/src/app/modules/shared/nav/nav.component.ts
+++ frontend/src/app/modules/shared/nav/nav.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { ThemeToggleComponent } from "../theme-toggle/theme-toggle.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
-  imports: [MatSidenavModule, ThemeToggleComponent],
+  imports: [MatSidenavModule, ThemeToggleComponent, RouterModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
